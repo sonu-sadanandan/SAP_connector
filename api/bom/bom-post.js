3f1ced97f6163b1c9078008a14d85bebdfb48888
abhinandan-mooralli <abhinandanbhat19@gmail.com>
2024-06-23 19:47:25 +0200

    Automated POST request from a CSV file: Reads the 1st row and creates material with the same name

--- api/bom/bom-post.js
+++ api/bom/bom-post.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
-// const qs = require('qs');
+const fs = require('fs');
+const csv = require('csv-parser');
+require('dotenv').config();
 
 // Basic Authentication credentials
 const username = process.env.USER_NAME;
@@ -9,36 +11,6 @@ const host = 'a20z.ucc.ovgu.de';  // Replace with your host
 // URLs for the endpoints
 const Material_URL = `https://${host}/sap/opu/odata/sap/API_PRODUCT_SRV/A_Product`;
 const BOM_URL = `https://${host}/sap/opu/odata/sap/API_BILL_OF_MATERIAL_SRV;v=0002/MaterialBOMItem`;
-
-// JSON body for the POST requests
-const Material_POST = {               
-    "Product": "APITEST1",
-    "ProductType": "HALB",
-    "IndustrySector": "M",
-    "BaseUnit": "EA",
-    "to_Description": [{
-        "Product": "APITEST1",
-        "Language": "EN",
-        "ProductDescription": "API test from POSTMAN"
-    }],
-    "to_Plant": [{
-        "Product": "APITEST1",
-        "Plant": "HH00"
-     }]
-};
-
-const BOM_POST = {
-    "BillOfMaterial": "00034033",
-    "BillOfMaterialCategory": "M",
-    "BillOfMaterialVariant": "1",
-    "BillOfMaterialItemNodeNumber": "1",
-    "Material": "53",
-    // "Plant": "HH00",
-    "BillOfMaterialComponent": "APITEST1",
-    "BillOfMaterialItemUnit": "EA",
-    "BillOfMaterialItemQuantity": "1",
-    "BillOfMaterialItemCategory": "L"
-};
 
 const auth = Buffer.from(`${username}:${password}`).toString('base64');
 
@@ -68,10 +40,52 @@ async function makePostRequest(url, data, csrfToken, cookies) {
   });
 }
 
-async function main() {
-  try {
+// Function to read CSV and perform POST requests
+async function processCSV() {
+    const results = [];
+  
+    fs.createReadStream('mbom-15.06.24.csv')
+      .pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', async () => {
+        // Assuming we only process the first row for this example
+        if (results.length > 0) {
+          const row = results[0];
+          const productDescription = row['Display Name'];
+          const product = row['Name(R)'].toUpperCase();
+// JSON body for the POST requests
+const Material_POST = {               
+    "Product": product,
+    "ProductType": "HALB",
+    "IndustrySector": "M",
+    "BaseUnit": "EA",
+    "to_Description": [{
+        "Product": product,
+        "Language": "EN",
+        "ProductDescription": productDescription
+    }],
+    "to_Plant": [{
+        "Product": product,
+        "Plant": "HH00"
+     }]
+};
+
+const BOM_POST = {
+    "BillOfMaterial": "00034033",
+    "BillOfMaterialCategory": "M",
+    "BillOfMaterialVariant": "1",
+    "BillOfMaterialItemNodeNumber": "1",
+    "Material": "53",
+    // "Plant": "HH00",
+    "BillOfMaterialComponent": product,
+    "BillOfMaterialItemUnit": "EA",
+    "BillOfMaterialItemQuantity": "1",
+    "BillOfMaterialItemCategory": "L"
+};
+
+try {
     const { csrfToken, cookies } = await fetchTokenAndCookies();
-    
+
     await makePostRequest(Material_URL, Material_POST, csrfToken, cookies);
     await makePostRequest(BOM_URL, BOM_POST, csrfToken, cookies);
 
@@ -80,5 +94,7 @@ async function main() {
     console.error('Error making POST requests:', error);
   }
 }
+});
+}
 
-main();
+processCSV();
